e35b3b7bf34a502352a3e548ffbd3ead19852f22
Aswin Sivadas <2001Aswin89@gmail.com>
2024-01-31 01:03:17 +0530

    1 testcase failed 4

--- python/nested_lists.py
+++ python/nested_lists.py
@@ -8,19 +8,29 @@ if __name__ == '__main__':
         l.append(score)
         lis.append(l)
     # print(lis)
-    largest=0
-    second=0
+    smallest=lis[0][1]
+    # second=
     for i in range(0, len(lis)):
-        if lis[i][1] >= second:
-            if lis[i][1] >= largest:
-                second=largest
-                largest=lis[i][1]
-            else:
-                second=lis[i][1]
-    # print(second, " ", largest)
-
+        if lis[i][1]>smallest:
+            second=lis[i][1]
+            break
+    for i in range(0, len(lis)):
+        # print(second)
+        # if lis[i][1] < second:
+        if lis[i][1] < smallest:
+            second=smallest
+            smallest=lis[i][1]
+        elif lis[i][1] <second and lis[i][1] > smallest:
+            second=lis[i][1]
+        
+    # print(second)
+ 
+    
+    list2=[]     
     for i in range(0, len(lis)):
         if lis[i][1] == second:
-            print(lis[i][0])
-
+            list2.append(lis[i][0])
+    list2.sort()
+    for i in list2:
+        print(i)
 
