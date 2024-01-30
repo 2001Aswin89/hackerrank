0422f90bded121979562bffdf414a254285c5aab
Aswin Sivadas <2001Aswin89@gmail.com>
2024-01-30 22:23:31 +0530

    displays second largest number

--- python/nested_lists.py
+++ python/nested_lists.py
@@ -1,4 +1,26 @@
 if __name__ == '__main__':
+    lis=[]
     for _ in range(int(input())):
+        l=[]
         name = input()
         score = float(input())
+        l.append(name)
+        l.append(score)
+        lis.append(l)
+    # print(lis)
+    largest=0
+    second=0
+    for i in range(0, len(lis)):
+        if lis[i][1] >= second:
+            if lis[i][1] >= largest:
+                second=largest
+                largest=lis[i][1]
+            else:
+                second=lis[i][1]
+    # print(second, " ", largest)
+
+    for i in range(0, len(lis)):
+        if lis[i][1] == second:
+            print(lis[i][0])
+
+
