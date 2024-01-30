820d711d641c6d0f6e333c4be6d4028fdc078486
Aswin Sivadas <2001Aswin89@gmail.com>
2024-01-30 21:36:40 +0530

    list comprehention completed

--- python/list_comprehention.py
+++ python/list_comprehention.py
@@ -3,4 +3,16 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    print(x ," ",y , " , ", z, " , ", n)
+    # print("[",end="")
+    lis=[]
+    for i in range(0,x+1):
+        for j in range(0,y+1):
+            for k in range (0,z+1):
+                l=[]
+                if i+j+k != n:
+                    # print("[",i ,",",j , ",", k, "]",end="" )
+                    l.append(i)
+                    l.append(j)
+                    l.append(k)
+                    lis.append(l)
+    print(lis)
