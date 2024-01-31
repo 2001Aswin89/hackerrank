719f2b73c6c8420515c4ae1b93143a410e0eaddf
Aswin Sivadas <2001Aswin89@gmail.com>
2024-02-01 00:54:12 +0530

    3 test case passed

--- python/sets_noIdea.py
+++ python/sets_noIdea.py
@@ -1,1 +1,26 @@
-print("hi")
+x, y = input("").split()
+# print(x,y)
+n=list(map(int, input("").split()))
+for _ in range(int(y)):
+    a = set(map(int, input("").split()))
+    # a.append(numbers)
+# a=set(map(int, input("").split()))
+b=set(map(int, input("").split()))
+# print(n,a,b)
+happiness=0
+# for i in a:
+#     if i in n:
+#         happiness+=1
+# for i in b:
+#     if i in n:
+#         happiness-=1
+# for i in n:
+#     if i in a:
+#         happiness+=1
+#         a.remove(i)
+#     if i in b:
+#         happiness-=1
+#         b.remove(i)
+# print(happiness)
+# print(" this is the sighn you are looking for")
+print(n,a,b)
