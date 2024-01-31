9b17f350e6c2f11e591b80ba8c94f899760a016a
Aswin Sivadas <2001Aswin89@gmail.com>
2024-02-01 01:00:42 +0530

    the way inputs are taken needs rectification

--- python/sets_noIdea.py
+++ python/sets_noIdea.py
@@ -1,11 +1,35 @@
 x, y = input("").split()
 # print(x,y)
-n=list(map(int, input("").split()))
-for _ in range(int(y)):
-    a = set(map(int, input("").split()))
-    # a.append(numbers)
-# a=set(map(int, input("").split()))
-b=set(map(int, input("").split()))
+# n=list(map(int, input("").split()))
+# for _ in range(int(y)):
+#     a = set(map(int, input("").split()))
+#     # a.append(numbers)
+# # a=set(map(int, input("").split()))
+# b=set(map(int, input("").split()))
+
+
+
+def input_set(prompt):
+    return set(map(int, input(prompt).split()))
+
+# Take input for integer 'n'
+n = int(input(""))
+# Take input for set 'a'
+a = input_set("")
+
+# Take input for set 'b'
+b = input_set("")
+
+
+# Print the sets and 'n'
+print("\nSets and 'n' value:")
+print("Set 'a':", a)
+print("Set 'b':", b)
+print("'n':", n)
+
+
+
+
 # print(n,a,b)
 happiness=0
 # for i in a:
